c24e24825e93b13f0f306b87dbfbc5a3f4a7ecd8
Amelia Lee <nonexistentcharacter@gmail.com>
2022-11-11 13:30:44 -0800

    aaaa

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use bevy::prelude::*;
-use std::{io::{self, Stdout}, thread, time::Duration};
+use std::{
+    error::Error,
+    io::{self, Stdout},
+    rc::Rc,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc, LockResult,
+    },
+    thread,
+    time::Duration,
+};
 use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout, Rect},
@@ -13,9 +23,33 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
-struct terminal_info {
+use signal_hook::{consts::SIGINT, iterator::Signals};
+
+struct TerminalData {
     terminal: Terminal<CrosstermBackend<Stdout>>,
-    backend: CrosstermBackend<Stdout>
+    game_closing: bool,
+}
+
+static closing: AtomicBool = AtomicBool::new(false);
+
+fn draw_term(terminal: &mut TerminalData) {
+    terminal.terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    });
+}
+
+fn foxloop(mut terminal: ResMut<TerminalData>) {
+    if terminal.game_closing || closing.load(Ordering::Relaxed) {
+        disable_raw_mode();
+        execute!(
+            terminal.terminal.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        );
+        terminal.terminal.show_cursor();
+    }
 }
 
 fn main() -> Result<(), io::Error> {
@@ -26,34 +60,27 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    let mut signals = Signals::new(&[SIGINT])?;
 
+    App::new()
+        //.add_plugins(DefaultPlugins)
+        .insert_resource(TerminalData {
+            terminal: terminal,
+            game_closing: false,
+        })
+        .add_system(foxloop)
+        .run();
+
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            println!("Received signal {:?}", sig);
+            closing.fetch_or(true, Ordering::Relaxed);
+        }
+    });
 
     thread::sleep(Duration::from_millis(30000));
 
     // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
 
     Ok(())
-}
-
-fn hello_world_system() {
-    //println!("stdoutinfo");
-
-}
-
-fn gametick(mut termref: ResMut<Terminal<CrosstermBackend<io::Stdout>>>) {
-    termref.draw(|f| {
-        let size = f.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        let size2 = Rect::new(4, 4, 40, 30);
-        let block2 = Block::default().title("Inner Block").borders(Borders::ALL);
-        f.render_widget(block, size);
-        //f.render_widget(block2, size2);
-    });
 }
